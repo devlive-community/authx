1d4fff7b46c774f033f6c341bf7d8e5693613171
shicheng <shicheng@ttxit.com>
2019-03-12 20:34:15 +0800

    add web layout

--- bootstack-web/src/main/angular/src/app/app.routing.module.ts
+++ bootstack-web/src/main/angular/src/app/app.routing.module.ts
@@ -1,7 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// router
+const routes: Routes = [
+  { path: '', loadChildren: './layout/layout.module#LayoutModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
